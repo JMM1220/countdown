2616fe0b8b67880956be4f1d0b9e3bb9b00a09fe
井元梅 <jingyuanmei@moredian.com>
2020-09-14 15:43:49 +0800

    函数命名优化，及创建utils工具文件夹

--- countdown/src/pages/CountDown.vue
+++ countdown/src/pages/CountDown.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="countdown-box">
-    <p v-if="isCountDown">距离活动结束还有 <span>{{day}}天</span><span>{{hour}}时</span><span>{{minute}}分</span><span>{{seconds}}秒</span></p>
+    <p v-if="isCountDown">距离活动结束还有 <span>{{countDownConfig.day}}天</span><span>{{countDownConfig.hour}}时</span><span>{{countDownConfig.minute}}分</span><span>{{countDownConfig.seconds}}秒</span></p>
     <slot v-else name="content"></slot>
   </div>
 </template>
 
 <script>
+import { transformTime } from '@/utils/time.js'
+
 // 开始时间，结束时间，开始倒计时，结束倒计时
 export default {
   name: 'CountDown',
@@ -16,9 +18,9 @@ export default {
     },
     end: {
       type: [String, Number],
-      default: new Date().getTime()
+      default: new Date().getTime() + 24 * 60 * 60 * 1000 * 3
     },
-    handlePropFunc: {
+    afterCountdown: {
       type: Function,
       default: () => {
         return false
@@ -28,10 +30,12 @@ export default {
   computed: {},
   data () {
     return {
-      day: 0,
-      hour: 0,
-      minute: 0,
-      seconds: 0,
+      countDownConfig: {
+        day: 0,
+        hour: 0,
+        minute: 0,
+        seconds: 0
+      },
       timer: null,
       allSeconds: 0,
       isCountDown: true // 是否还在倒计时
@@ -66,23 +70,11 @@ export default {
       if (this.allSeconds === 0) {
         this.isCountDown = false
         clearInterval(this.timer)
-        this.handlePropFunc && this.handlePropFunc()
+        this.afterCountdown && this.afterCountdown()
         return
       }
       // 将时间戳转化为时间格式
-      this.transformTime(this.allSeconds)
-    },
-    transformTime (allSeconds) {
-      let _allSeconds = allSeconds
-      let _day = Math.floor(_allSeconds / 60 / 60 / 24) // 天
-      let _hour = Math.floor((_allSeconds - _day * 60 * 60 * 24) / (60 * 60)) // 小时
-      let _minutes = Math.floor((_allSeconds - _day * 60 * 60 * 24 - _hour * 60 * 60) / 60) // 分钟
-      let _seconds = _allSeconds - _day * 60 * 60 * 24 - _hour * 60 * 60 - _minutes * 60 // 秒
-
-      this.day = _day
-      this.hour = _hour < 10 ? `0${_hour}` : _hour
-      this.minutes = _minutes < 10 ? `0${_minutes}` : _minutes
-      this.seconds = _seconds < 10 ? `0${_seconds}` : _seconds
+      this.countDownConfig = transformTime(this.allSeconds)
     }
   }
 }
